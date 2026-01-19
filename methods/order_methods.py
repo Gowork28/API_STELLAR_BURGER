98c31b13e5bc0c0134d3b22eef527146b30743d2
Hanna Kulpo <anya.kulpo.1999@gmail.com>
2026-01-19 19:36:22 +0800

    Fixed

--- methods/order_methods.py
+++ methods/order_methods.py
@@ -5,7 +5,7 @@ from data.urls import UrlsForTest
 class OrderMethods:
 
     @staticmethod
-    @allure.title("Создание заказа")
+    @allure.step("Создание заказа")
     def create_order(token, body):
         header = {"Authorization":token}
         return requests.post(url=UrlsForTest.CREATE_ORDER_URL, headers=header, json=body)
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,8 @@
-import random
 import json
-
 import allure
 import pytest
-import string
+
+from helpers import generate_random_user_body
 
 from data.test_data import TestUserData
 from methods.user_methods import UserMethods
@@ -11,18 +10,8 @@ from methods.user_methods import UserMethods
 
 @pytest.fixture
 def unique_user():
-    with allure.step("Генератор рандомных данных"):
-        def generate_random_string(length):
-            letters = string.ascii_lowercase
-            random_string = ''.join(random.choice(letters) for i in range(length))
-            return random_string
-
     with allure.step("Собираем тело запроса с рандомными данными"):
-        body = {}
-
-        body["email"] = f"{generate_random_string(10)}@random.com"
-        body["password"] = generate_random_string(10)
-        body["name"] = generate_random_string(10)
+        body = generate_random_user_body()
 
         yield body
 
@@ -46,18 +35,8 @@ def exist_user():
 
 @pytest.fixture
 def auth_user():
-    with allure.step("Генератор рандомных данных"):
-        def generate_random_string(length):
-            letters = string.ascii_lowercase
-            random_string = ''.join(random.choice(letters) for i in range(length))
-            return random_string
-
     with allure.step("Собираем тело запроса с рандомными данными"):
-        body = {}
-
-        body["email"] = f"{generate_random_string(10)}@random.com"
-        body["password"] = generate_random_string(10)
-        body["name"] = generate_random_string(10)
+        body = generate_random_user_body()
 
     with allure.step("Создаем пользователя и получаем токен из ответа"):
         UserMethods.create_user(body)
